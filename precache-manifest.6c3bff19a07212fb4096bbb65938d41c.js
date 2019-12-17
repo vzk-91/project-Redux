d9aaa8740758122df094af6bddf825d8c3d2b213
Vahe Karapetyan <vzkarapetyan@gmail.com>
2019-12-17 23:00:15 +0400

    Updates

---
+++ precache-manifest.6c3bff19a07212fb4096bbb65938d41c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "871b0828485fa588a66a3fae1aadd0c5",
+    "url": "/project-Redux/index.html"
+  },
+  {
+    "revision": "65c8d309958c15067cbb",
+    "url": "/project-Redux/static/css/2.7aa6f31f.chunk.css"
+  },
+  {
+    "revision": "081570d8adea057a6b50",
+    "url": "/project-Redux/static/css/main.97e514fa.chunk.css"
+  },
+  {
+    "revision": "65c8d309958c15067cbb",
+    "url": "/project-Redux/static/js/2.609920a1.chunk.js"
+  },
+  {
+    "revision": "081570d8adea057a6b50",
+    "url": "/project-Redux/static/js/main.61078d93.chunk.js"
+  },
+  {
+    "revision": "5569538058f9b880d2a2",
+    "url": "/project-Redux/static/js/runtime-main.b67f91e4.js"
+  }
+]);
